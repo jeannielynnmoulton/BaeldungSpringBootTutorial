17d7293863ce375325dd7038c403579199c1c244
Jeannie Moulton <jeannie.moulton@diffblue.com>
2020-04-24 13:14:36 +0100

    Replace autowired

--- src/main/java/com/example/demo/controller/book/BookController.java
+++ src/main/java/com/example/demo/controller/book/BookController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller.book;
 import com.example.demo.persistence.model.Book;
 import com.example.demo.persistence.repo.BookRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/books")
 public class BookController {
 
+  @Autowired
   private BookRepository bookRepository;
 
   @GetMapping
