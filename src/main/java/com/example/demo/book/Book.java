b6c05751f64ecdca59d58f5f07ea6f2e2d387159
Jeannie Moulton <jeannie.moulton@diffblue.com>
2020-04-24 12:06:48 +0100

    Step 9: Add remaining getters and setters
    Again, tutorial doesn't say to do this.

--- src/main/java/com/example/demo/book/Book.java
+++ src/main/java/com/example/demo/book/Book.java
@@ -22,4 +22,24 @@ public class Book {
   public long getId() {
     return id;
   }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
 }
